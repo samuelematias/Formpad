32dc62c2ef250ec320004d5c617304a0d135aa90
samuelematias <hi@samuelematias.com>
2020-01-31 20:30:10 -0300

    Update icon path

--- lib/src/core/constants/assets/commons.dart
+++ lib/src/core/constants/assets/commons.dart
@@ -1,5 +1,5 @@
 import 'package:Formpad/src/core/constants/assets/prefix.dart';
 
 const String formpadLogo = '${PREFIX}formpad-logo.png';
-const String iconMenu = '${PREFIX}icon-menu.png';
-const String iconGithub = '${PREFIX}icon-github.png';
+const String iconMenu = '${PREFIX}icons/icon-menu.png';
+const String iconGithub = '${PREFIX}icons/icon-github.png';
