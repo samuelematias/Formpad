3ce1b2a52b632602a20c376649762c5a05e84b99
samuelematias <hi@samuelematias.com>
2020-01-31 20:31:39 -0300

    Update Texts Extension-method

--- lib/src/core/theme/texts.dart
+++ lib/src/core/theme/texts.dart
@@ -1,65 +1,124 @@
 import 'package:Formpad/src/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 
-extension TextStyles on Widget {
-  Widget h1() => Text(
-        '$this',
+extension TextStyles on Text {
+  Text h1() {
+    if (this is Text) {
+      final Text t = this;
+      return Text(
+        t.data,
         style: TextStyle(
           color: ColorTheme.white80,
           fontSize: 14,
         ),
       );
+    }
+    return null;
+  }
 
-  Widget h2() => Text(
-        '$this',
+  Text h2() {
+    if (this is Text) {
+      final Text t = this;
+      return Text(
+        t.data,
         style: TextStyle(
           color: ColorTheme.white,
           fontSize: 50,
           fontWeight: FontWeight.bold,
         ),
       );
+    }
+    return null;
+  }
 
-  Widget p1() => Text(
-        '$this',
+  Text p1() {
+    if (this is Text) {
+      final Text t = this;
+      return Text(
+        t.data,
         style: TextStyle(
           color: ColorTheme.whiteCC,
           fontSize: 16,
         ),
       );
+    }
+    return null;
+  }
 
-  Widget b1() => Text(
-        '$this',
+  Text b1() {
+    if (this is Text) {
+      final Text t = this;
+      return Text(
+        t.data,
         style: TextStyle(
           color: ColorTheme.white,
           fontSize: 15,
         ),
       );
+    }
+    return null;
+  }
 
-  Widget n1() => Text(
-        '$this',
+  Text n1() {
+    if (this is Text) {
+      final Text t = this;
+      return Text(
+        t.data,
         style: TextStyle(
           color: ColorTheme.white,
           fontSize: 14,
           fontWeight: FontWeight.bold,
         ),
       );
+    }
+    return null;
+  }
 
-  Widget n2() => Text(
-        '$this',
+  Text n2() {
+    if (this is Text) {
+      final Text t = this;
+      return Text(
+        t.data,
         style: TextStyle(
           color: ColorTheme.black,
           fontSize: 14,
           fontWeight: FontWeight.bold,
         ),
       );
+    }
+    return null;
+  }
+}
 
-  Widget u1() => Text(
-        '$this',
+extension TextSpanStyles on TextSpan {
+  TextSpan span1() {
+    if (this is TextSpan) {
+      final TextSpan t = this;
+      return TextSpan(
+        text: t.text,
         style: TextStyle(
-          color: ColorTheme.accent,
+          color: ColorTheme.white,
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+    }
+    return null;
+  }
+
+  TextSpan span2() {
+    if (this is TextSpan) {
+      final TextSpan t = this;
+      return TextSpan(
+        text: t.text,
+        style: TextStyle(
+          color: Colors.red,
           fontSize: 14,
           decoration: TextDecoration.underline,
           fontWeight: FontWeight.w800,
         ),
       );
+    }
+    return null;
+  }
 }
