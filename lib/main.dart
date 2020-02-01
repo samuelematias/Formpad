8a4e80761c85b77707d57ff40d47289aa1f13325
samuelematias <hi@samuelematias.com>
2020-02-01 19:59:12 -0300

    Add config to transparent statusBar on Mobile

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:Formpad/src/core/routes/router.dart';
 import 'package:Formpad/src/core/theme/theme.dart';
 import 'package:Formpad/src/ui/landing/landing_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: proximaNova),
