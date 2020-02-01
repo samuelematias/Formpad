31e222af4269771df15fb0bfe8861c8953bc575f
samuelematias <hi@samuelematias.com>
2020-02-01 10:42:18 -0300

    Add new icons

--- lib/src/core/constants/assets/commons.dart
+++ lib/src/core/constants/assets/commons.dart
@@ -1,5 +1,8 @@
 import 'package:Formpad/src/core/constants/assets/prefix.dart';
 
 const String formpadLogo = '${PREFIX}formpad-logo.png';
+const String formpadLogoBlack = '${PREFIX}formpad-logo-black.png';
 const String iconMenu = '${PREFIX}icons/icon-menu.png';
+const String iconMenuBlack = '${PREFIX}icons/icon-menu-black.png';
 const String iconGithub = '${PREFIX}icons/icon-github.png';
+const String iconGithubBlack = '${PREFIX}icons/icon-github-black.png';
