f9a5b69d54b0629d6d466b207c7cca1ecaf4b828
samuelematias <hi@samuelematias.com>
2020-02-01 19:00:58 -0300

    update DefaultButton widget

--- lib/src/widgets/button/default_button.dart
+++ lib/src/widgets/button/default_button.dart
@@ -2,13 +2,24 @@ import 'package:Formpad/src/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class DefaultButton extends StatelessWidget {
+  final String label;
+  final Function onPressed;
+  final bool active;
+  final bool loading;
+  const DefaultButton({
+    @required this.label,
+    this.onPressed,
+    this.active = true,
+    this.loading = false,
+  });
+
   @override
   Widget build(BuildContext context) {
     return FlatButton(
-      onPressed: () {},
+      onPressed: active && !loading ? onPressed : null,
       color: ColorTheme.green,
       child: Container(
-        child: Text("Acessar o Form").b1(),
+        child: Text(label).b1(),
       ).paddingAll(Space.dodger_blue),
     );
   }
