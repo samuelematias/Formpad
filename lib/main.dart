ed3faee1b48e49b907574b91798ac612623dd90d
samuelematias <hi@samuelematias.com>
2020-02-01 22:52:36 -0300

    remove unnecessary params

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: proximaNova),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
       home: HomePage(),
       initialRoute: '/',
       onGenerateRoute: Router.generateRoute,
